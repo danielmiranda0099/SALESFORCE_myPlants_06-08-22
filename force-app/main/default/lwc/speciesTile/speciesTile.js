72ec54d34f89f2c84602a7ae0162404efa1ae468
Daniel Miranda <danielmesa877@gmail.com>
2022-08-06 13:55:38 -0500

    template if:true, componentes base, servicio de navegación

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,6 +1,27 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class SpeciesTile extends LightningElement {
+export default class SpeciesTile extends NavigationMixin(LightningElement) {
     myProperty = "Esto es una propiedad de un web component";
     @api specie;
+
+    get isOutdoors(){
+        return this.specie.Location__c.includes("Outdoors");
+    }
+
+    get isIndoors(){
+        return this.specie.Location__c.includes("Indoors");
+    }
+
+    navigateToRecordViewPage() {
+        // View a custom object record.
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.specie.Id,
+                objectApiName: 'Species__c', // objectApiName is optional
+                actionName: 'view'
+            }
+        });
+    }
 }
