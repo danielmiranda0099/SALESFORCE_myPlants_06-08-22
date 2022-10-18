b4d9278f1aefed8df160f7fcbb92910ab6ecf840
Daniel Miranda <danielmesa877@gmail.com>
2022-10-18 12:27:24 -0500

    [ADD] web components base

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+export default class SpeciesTile extends LightningElement {
+  specie = {
+    Name: "Jazmin",
+    Description: "Olorosa y bonita plata trepadora",
+    Image_URL__c:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPE-QW-Ap9x-2JmjpBrC99317VkpVwzDHONzK3mxH24VZ4LGPbC6ImgqQHC975GPnzV5Y&usqp=CAU",
+    Location__c: "Indoors, Outdoors"
+  };
+}
